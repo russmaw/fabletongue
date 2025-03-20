fe09e80acebfe523d7e8a4689c483b4fdebd2dfd
russmaw <russmaw@gmail.com>
2025-03-20 17:24:29 -0400

    Update project configuration with latest dependencies and best practices

---
+++ web/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.jest.json',
+    }],
+  },
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/.next/',
+    '/coverage/',
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/.next/',
+  ],
+} 
